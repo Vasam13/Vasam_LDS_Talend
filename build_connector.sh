0d397efafaffa41731b944f5d6b7bf8732d77056
Vasam <venkatesh.vasam13@gmail.com>
2022-11-24 17:59:04 +0530

    Minor updates

--- build_connector.sh
+++ build_connector.sh
@@ -14,7 +14,7 @@ mvn clean compile assembly:single &&
 echo "Step 4: ${MODULE}-connector-1.0-SNAPSHOT-jar-with-dependencies.jar is successfully generated!" &&
 cp target/${MODULE}-connector-1.0-SNAPSHOT-jar-with-dependencies.jar . &&
 echo 'Step 5: Packaging the connector' &&
-sudo ./package_connector.sh -m ${MODULE} -j ${MODULE}-connector-1.0-SNAPSHOT-jar-with-dependencies.jar -f Dockerfile -d sdk-3.10.0-jar-with-dependencies.jar &&
+sudo ./package_connector.sh -m ${MODULE} -j ${MODULE}-connector-1.0-SNAPSHOT-jar-with-dependencies.jar -f Dockerfile -d sdk-4.1.4-jar-with-dependencies.jar &&
 rm -rf ${MODULE}-connector-1.0-SNAPSHOT-jar-with-dependencies.jar &&
 rm -rf MANIFEST.MF &&
 echo "${MODULE} connector is successfully created as alation-${MODULE}.zip"
